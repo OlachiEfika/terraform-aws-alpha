3d603eda6e3bc15c4e6d3172dd1f9ff1abde9bd8
Olachi Efika <olachi_efika@yahoo.com>
2023-02-08 23:08:11 -0500

    Create public & private instances

--- instance.tf
+++ instance.tf
@@ -35,3 +35,40 @@ resource "aws_instance" "public" {
     Name = "${var.env_code}-public"
   }
 }
+
+resource "aws_security_group" "private" {
+  name        = "${var.env_code}-private"
+  description = "Private security group"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_code}-private"
+  }
+}
+
+resource "aws_instance" "private" {
+  ami                         = "ami-0aa7d40eeae50c9a9"
+  instance_type               = "t2.micro"
+  key_name                    = "devops"
+  vpc_security_group_ids      = [aws_security_group.private.id]
+  subnet_id                   = aws_subnet.private[0].id
+
+  tags = {
+    Name = "${var.env_code}-private"
+  }
+}
