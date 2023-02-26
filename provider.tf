cc5416360e2ae95b814ce2bdfefcd7aedc1cca53
Olachi Efika <olachi_efika@yahoo.com>
2023-02-26 17:28:06 -0500

    Add state lock

--- provider.tf
+++ provider.tf
@@ -1,7 +1,8 @@
 terraform {
     backend "s3" {
-        bucket  = "my-terraform-tf-state-bucket"
-        key     = "terraform.tfstate"
-        region  = "ca-central-1"
+        bucket         = "my-terraform-tf-state-bucket"
+        key            = "terraform.tfstate"
+        region         = "ca-central-1"
+        dynamodb_table = "my-terraform-remote-state-lock"
     }
 }
